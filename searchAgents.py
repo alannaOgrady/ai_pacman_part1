17df39482246364e839c79a9709ec1b3ee2ac7f4
Alanna O'Grady <alannasogrady@gmail.com>
2019-02-18 00:19:59 +0000

    on q5, corners prob, currently getting stuck in 1 corner, think i have to improve heuristic funcion

--- searchAgents.py
+++ searchAgents.py
@@ -266,6 +266,7 @@ def euclideanHeuristic(position, problem, info={}):
 # This portion is incomplete.  Time to write code!  #
 #####################################################
 
+
 class CornersProblem(search.SearchProblem):
     """
     This search problem finds paths through all four corners of a layout.
@@ -289,12 +290,16 @@ class CornersProblem(search.SearchProblem):
         # in initializing the problem
         "*** YOUR CODE HERE ***"
 
+
     def getStartState(self):
         """
         Returns the start state (in your state space, not the full Pacman state
         space)
         """
         "*** YOUR CODE HERE ***"
+        #get pacman starting position(state)
+        #return state and corners visited
+        return (self.startingPosition, [])
         util.raiseNotDefined()
 
     def isGoalState(self, state):
@@ -302,6 +307,19 @@ class CornersProblem(search.SearchProblem):
         Returns whether this search state is a goal state of the problem.
         """
         "*** YOUR CODE HERE ***"
+        #have corner llocations been visited
+        #is current state a corner?
+        current_position = state[0]
+        visited_corners = state[1]
+        if current_position in self.corners:
+            #has it been visited?
+            if current_position not in visited_corners:
+                visited_corners.append(current_position)
+        if len(visited_corners) == len(self.corners):
+            return True
+        else:
+            return False
+
         util.raiseNotDefined()
 
     def getSuccessors(self, state):
@@ -314,15 +332,18 @@ class CornersProblem(search.SearchProblem):
             state, 'action' is the action required to get there, and 'stepCost'
             is the incremental cost of expanding to that successor
         """
-
+        current_position = state[0]
+        visited_corners = state[1]
         successors = []
         for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
             # Add a successor state to the successor list if the action is legal
             # Here's a code snippet for figuring out whether a new position hits a wall:
-            #   x,y = currentPosition
-            #   dx, dy = Actions.directionToVector(action)
-            #   nextx, nexty = int(x + dx), int(y + dy)
-            #   hitsWall = self.walls[nextx][nexty]
+            x,y = current_position
+            dx, dy = Actions.directionToVector(action)
+            nextx, nexty = int(x + dx), int(y + dy)
+            hitsWall = self.walls[nextx][nexty]
+            if not hitsWall:
+                successors.append((((nextx, nexty), visited_corners), action, 1))
 
             "*** YOUR CODE HERE ***"
 
@@ -359,8 +380,17 @@ def cornersHeuristic(state, problem):
     corners = problem.corners # These are the corner coordinates
     walls = problem.walls # These are the walls of the maze, as a Grid (game.py)
 
+    x1, y1 = state[0]
+    visited_corners = state[1]
+    distances_to_corners = []
     "*** YOUR CODE HERE ***"
-    return 0 # Default to trivial solution
+    for corner in corners:
+        #check if it has not been visited
+        if corner not in visited_corners:
+            x2, y2 = corner
+            man_dist = abs(x1 - x2) + abs(y1 - y2)
+            distances_to_corners.append(man_dist)
+    return min(distances_to_corners) # Default to trivial solution
 
 class AStarCornersAgent(SearchAgent):
     "A SearchAgent for FoodSearchProblem using A* and your foodHeuristic"
